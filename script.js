2b4059bb4ec3c9b54979d593edab9a0c08ea1f05
Qairkhan <shyngys94@gmail.com>
2020-03-11 17:15:30 +0300

    lesson11

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ tratratra = document.getElementById(blablabla);
 window.alert(tratratra.value);
 var classs;
 classs=('4ina')
+
+
+
+function alertvalue() {
+    console.log ('1');
+    console.log ('2');
+    console.log ('3');
+    console.log ('4');
+    
+}
+
+alertvalue();
