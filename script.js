1f43a6831983c5202438d847dfb10975e4e90278
Qairkhan <shyngys94@gmail.com>
2020-03-11 18:10:19 +0300

    lesson14

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-var tratratra;
-var blablabla;
-blablabla='input';
-tratratra = document.getElementById(blablabla);
-window.alert(tratratra.value);
-var classs;
-classs=('4ina')
-
-
-
-function alertvalue() {
-    console.log ('1');
-    console.log ('2');
-    console.log ('3');
-    console.log ('4');
-    
+function addErrorClass(elementId) {
+    var element = document.getElementById(elementId);
+    element.className = "error-input";
 }
 
-alertvalue();
+function addErrorClassToAllInputs() {
+    addErrorClass ("first-name") ;   
+    addErrorClass ("last-name")  ;  
+    addErrorClass ("address")    ;
+}
+
+addErrorClassToAllInputs();
+// var tratratra;
+// var blablabla;
+// blablabla='input';
+// tratratra = document.getElementById(blablabla);
+// window.alert(tratratra.value);
+// var classs;
+// classs=('4ina')
+
+
+
+// function alertvalue() {
+//     console.log ('1');
+//     console.log ('2');
+//     console.log ('3');
+//     console.log ('4');
+    
+// }
+
+// alertvalue();
