870d38bfb20e8ea9a3a72776e657fa5ba480e856
Qairkhan <shyngys94@gmail.com>
2020-03-12 17:13:15 +0300

    lesson17

--- script2.js
+++ script2.js
@@ -2,13 +2,24 @@ var buttonPlus = document.getElementById('buttonPlus');
 var buttonMinus = document.getElementById('buttonMinus');
 var buttonMulti = document.getElementById('buttonMulti');
 var buttonDel = document.getElementById('buttonDel');
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+
+function getNumber1() {
+    return Number (input1.value);
+    
+}
+function getNumber2() {
+    return Number (input2.value);
+    
+}
+
 
 function onButtonPlusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
+   
 
-    var number1 = Number (input1.value);
-    var number2 = Number (input2.value);
+    var number1 = getNumber1();
+    var number2 = getNumber2();
 
     var summ = number1 + number2;
     window.alert(summ);
