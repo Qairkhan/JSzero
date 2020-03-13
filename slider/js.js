56ba84c6d23f4fd93e1283cf650afdadb3c8f8d3
Qairkhan <shyngys94@gmail.com>
2020-03-13 19:05:48 +0300

    slyder 3

--- slider/js.js
+++ slider/js.js
@@ -13,18 +13,31 @@ var imagesUrls = [];
 imagesUrls.push('https://muz-tv.ru/storage/pic/e/2/e289a15ed2db453e1ecbf87ad157e48e.jpg');
 imagesUrls.push('https://e3.365dm.com/19/04/768x432/skynews-bts-number-one_4644694.jpg?20190419153417');
 imagesUrls.push('https://tntmusic.ru/media/content/article/2019-10-14_08-09-53__0059a8f6-ee5a-11e9-a31d-f1ee0cd08588.jpg');
-imagesUrls.push('https://интересное.ru.com/wp-content/uploads/2019/06/6-faktov-o-tom-chto-takoe-K-Pop-i-pochemu-vse-shodyat-po-nemu-s-uma-1.jpg');
+imagesUrls.push('https://www.k-pop.ru/wp-content/uploads/2019/01/unnamed-32.jpg');
 
 var currentImageIndex = 0;
 slideImage.src = imagesUrls[currentImageIndex];
-
+showPrevBtn.disabled = true;
 
 // function definition
 function onShowPrevBtnClick() {
     currentImageIndex--;
     slideImage.src = imagesUrls[currentImageIndex];
+    showNextBtn.disabled = false;
+
+    // disable prev btn
+    if (currentImageIndex === 0 ) {
+        showPrevBtn.disabled = true;
+    }
 }
 function onShowNextBtnClick() {
     currentImageIndex++;
+    
     slideImage.src = imagesUrls[currentImageIndex];
+    showPrevBtn.disabled = false;
+
+// disable next btn
+    if (currentImageIndex === (imagesUrls.length -1) ) {
+        showNextBtn.disabled = true;
+    }
 }
