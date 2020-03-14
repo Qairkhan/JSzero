79bab6b4226a50817869d6af873875db64726c8a
Qairkhan <shyngys94@gmail.com>
2020-03-14 12:14:09 +0300

    slider to object

--- slider/js.js
+++ slider/js.js
@@ -2,7 +2,8 @@
 kpop.start();
 
 
-
+var imagesUrls = [];
+var currentImageIndex = 0;
 //find elements
 var showPrevBtn = document.getElementById('show-prev-btn');
 var showNextBtn = document.getElementById('show-next-btn');
@@ -13,19 +14,19 @@ showPrevBtn.addEventListener('click', onShowPrevBtnClick);
 showNextBtn.addEventListener('click', onShowNextBtnClick);
 
 // images arrays
-var imagesUrls = [];
+
 
 imagesUrls.push('https://muz-tv.ru/storage/pic/e/2/e289a15ed2db453e1ecbf87ad157e48e.jpg');
 imagesUrls.push('https://e3.365dm.com/19/04/768x432/skynews-bts-number-one_4644694.jpg?20190419153417');
 imagesUrls.push('https://tntmusic.ru/media/content/article/2019-10-14_08-09-53__0059a8f6-ee5a-11e9-a31d-f1ee0cd08588.jpg');
 imagesUrls.push('https://www.k-pop.ru/wp-content/uploads/2019/01/unnamed-32.jpg');
 
-var currentImageIndex = 0;
+
 slideImage.src = imagesUrls[currentImageIndex];
 showPrevBtn.disabled = true;
 
 // function definition
-function onShowPrevBtnClick() {
+function onShowPrevBtnClick(event) {
     currentImageIndex--;
     slideImage.src = imagesUrls[currentImageIndex];
     showNextBtn.disabled = false;
@@ -35,7 +36,7 @@ function onShowPrevBtnClick() {
         showPrevBtn.disabled = true;
     }
 }
-function onShowNextBtnClick() {
+function onShowNextBtnClick(event) {
     currentImageIndex++;
     
     slideImage.src = imagesUrls[currentImageIndex];
---
+++ slider/slider.js
@@ -0,0 +1,7 @@
+var slider1 = {
+    imagesUrls = [],
+    currentImageIndex = 0,
+    showPrevBtn = document.getElementById('show-prev-btn'),
+    showNextBtn = document.getElementById('show-next-btn'),
+    slideImage = document.getElementById('slide-img'),
+}
