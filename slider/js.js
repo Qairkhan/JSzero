12eca93bd732985f59b8f478e73354d7e16c9f34
Qairkhan <shyngys94@gmail.com>
2020-03-14 12:34:35 +0300

    object=>this2

--- slider/js.js
+++ slider/js.js
@@ -1,49 +1,50 @@
-//kpop object
-kpop.start();
+slider1.start();
+// //kpop object
+// kpop.start();
 
 
-var imagesUrls = [];
-var currentImageIndex = 0;
-//find elements
-var showPrevBtn = document.getElementById('show-prev-btn');
-var showNextBtn = document.getElementById('show-next-btn');
-var slideImage = document.getElementById('slide-img');
+// var imagesUrls = [];
+// var currentImageIndex = 0;
+// //find elements
+// var showPrevBtn = document.getElementById('show-prev-btn');
+// var showNextBtn = document.getElementById('show-next-btn');
+// var slideImage = document.getElementById('slide-img');
 
-//addlistiner
-showPrevBtn.addEventListener('click', onShowPrevBtnClick);
-showNextBtn.addEventListener('click', onShowNextBtnClick);
+// //addlistiner
+// showPrevBtn.addEventListener('click', onShowPrevBtnClick);
+// showNextBtn.addEventListener('click', onShowNextBtnClick);
 
-// images arrays
-
-
-imagesUrls.push('https://muz-tv.ru/storage/pic/e/2/e289a15ed2db453e1ecbf87ad157e48e.jpg');
-imagesUrls.push('https://e3.365dm.com/19/04/768x432/skynews-bts-number-one_4644694.jpg?20190419153417');
-imagesUrls.push('https://tntmusic.ru/media/content/article/2019-10-14_08-09-53__0059a8f6-ee5a-11e9-a31d-f1ee0cd08588.jpg');
-imagesUrls.push('https://www.k-pop.ru/wp-content/uploads/2019/01/unnamed-32.jpg');
+// // images arrays
 
 
-slideImage.src = imagesUrls[currentImageIndex];
-showPrevBtn.disabled = true;
+// imagesUrls.push('https://muz-tv.ru/storage/pic/e/2/e289a15ed2db453e1ecbf87ad157e48e.jpg');
+// imagesUrls.push('https://e3.365dm.com/19/04/768x432/skynews-bts-number-one_4644694.jpg?20190419153417');
+// imagesUrls.push('https://tntmusic.ru/media/content/article/2019-10-14_08-09-53__0059a8f6-ee5a-11e9-a31d-f1ee0cd08588.jpg');
+// imagesUrls.push('https://www.k-pop.ru/wp-content/uploads/2019/01/unnamed-32.jpg');
 
-// function definition
-function onShowPrevBtnClick(event) {
-    currentImageIndex--;
-    slideImage.src = imagesUrls[currentImageIndex];
-    showNextBtn.disabled = false;
 
-    // disable prev btn
-    if (currentImageIndex === 0 ) {
-        showPrevBtn.disabled = true;
-    }
-}
-function onShowNextBtnClick(event) {
-    currentImageIndex++;
+// slideImage.src = imagesUrls[currentImageIndex];
+// showPrevBtn.disabled = true;
+
+// // function definition
+// function onShowPrevBtnClick(event) {
+//     currentImageIndex--;
+//     slideImage.src = imagesUrls[currentImageIndex];
+//     showNextBtn.disabled = false;
+
+//     // disable prev btn
+//     if (currentImageIndex === 0 ) {
+//         showPrevBtn.disabled = true;
+//     }
+// }
+// function onShowNextBtnClick(event) {
+//     currentImageIndex++;
     
-    slideImage.src = imagesUrls[currentImageIndex];
-    showPrevBtn.disabled = false;
+//     slideImage.src = imagesUrls[currentImageIndex];
+//     showPrevBtn.disabled = false;
 
-// disable next btn
-    if (currentImageIndex === (imagesUrls.length -1) ) {
-        showNextBtn.disabled = true;
-    }
-}
+// // disable next btn
+//     if (currentImageIndex === (imagesUrls.length -1) ) {
+//         showNextBtn.disabled = true;
+//     }
+// }
--- slider/slider.js
+++ slider/slider.js
@@ -1,24 +1,24 @@
 var slider1 = {
-    imagesUrls = [],
-    currentImageIndex = 0,
-    showPrevBtn = document.getElementById('show-prev-btn'),
-    showNextBtn = document.getElementById('show-next-btn'),
-    slideImage = document.getElementById('slide-img'),
+    imagesUrls: [],
+    currentImageIndex: 0,
+    showPrevBtn: document.getElementById('show-prev-btn'),
+    showNextBtn: document.getElementById('show-next-btn'),
+    slideImage: document.getElementById('slide-img'),
 
     start: function() {
     //addlistiner
-    showPrevBtn.addEventListener('click', onShowPrevBtnClick);
-    showNextBtn.addEventListener('click', onShowNextBtnClick);
+    this.showPrevBtn.addEventListener('click', this.onShowPrevBtnClick);
+    this.showNextBtn.addEventListener('click', this.onShowNextBtnClick);
 
     // images arrays
-    imagesUrls.push('https://muz-tv.ru/storage/pic/e/2/e289a15ed2db453e1ecbf87ad157e48e.jpg');
-    imagesUrls.push('https://e3.365dm.com/19/04/768x432/skynews-bts-number-one_4644694.jpg?20190419153417');
-    imagesUrls.push('https://tntmusic.ru/media/content/article/2019-10-14_08-09-53__0059a8f6-ee5a-11e9-a31d-f1ee0cd08588.jpg');
-    imagesUrls.push('https://www.k-pop.ru/wp-content/uploads/2019/01/unnamed-32.jpg');
+    this.imagesUrls.push('https://muz-tv.ru/storage/pic/e/2/e289a15ed2db453e1ecbf87ad157e48e.jpg');
+    this.imagesUrls.push('https://e3.365dm.com/19/04/768x432/skynews-bts-number-one_4644694.jpg?20190419153417');
+    this.imagesUrls.push('https://tntmusic.ru/media/content/article/2019-10-14_08-09-53__0059a8f6-ee5a-11e9-a31d-f1ee0cd08588.jpg');
+    this.imagesUrls.push('https://www.k-pop.ru/wp-content/uploads/2019/01/unnamed-32.jpg');
 
 
-    slideImage.src = imagesUrls[currentImageIndex];
-    showPrevBtn.disabled = true;
+    this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+    this.showPrevBtn.disabled = true;
     },
 
     // function definition
