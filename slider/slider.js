a7f1ec39d057337cd6ab7f96d9f3dc3c68612afc
Qairkhan <shyngys94@gmail.com>
2020-03-14 12:16:55 +0300

    slider to object2

--- slider/slider.js
+++ slider/slider.js
@@ -4,4 +4,27 @@ var slider1 = {
     showPrevBtn = document.getElementById('show-prev-btn'),
     showNextBtn = document.getElementById('show-next-btn'),
     slideImage = document.getElementById('slide-img'),
+
+    // function definition
+    onShowPrevBtnClick: function (event) {
+        currentImageIndex--;
+        slideImage.src = imagesUrls[currentImageIndex];
+        showNextBtn.disabled = false;
+
+        // disable prev btn
+        if (currentImageIndex === 0 ) {
+            showPrevBtn.disabled = true;
+        }
+    },
+    onShowNextBtnClick: function (event) {
+        currentImageIndex++;
+        
+        slideImage.src = imagesUrls[currentImageIndex];
+        showPrevBtn.disabled = false;
+    
+        // disable next btn
+        if (currentImageIndex === (imagesUrls.length -1) ) {
+            showNextBtn.disabled = true;
+        }
+    },
 }
